0ee06ef0829dd3606aecce15563de2e4754576ed
Maksim Panasenko <maks.panasenko01@bk.ru>
2020-06-30 21:08:07 +0300

    Create DataManager

---
+++ SalesStatistics/Data.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace SalesStatistics
+{
+    public class Data
+    {
+        public DayData[] daysData { get; set; }
+    }
+
+    public class DayData
+    {
+        public DateTime Day { get; set; }
+
+        public decimal Profit { get; set; }
+    }
+}
